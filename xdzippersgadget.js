ab494b6cc1305db93a6e56c3934675f226db6899
xdzipper-alt <xdzipperlol89@outlook.com>
2025-01-29 21:43:08 -0500

    Create xdzippersgadget.js

---
+++ xdzippersgadget.js
@@ -0,0 +1,7 @@
+/* This is one of XDZIPPER's Gadgets. This one with the ID of 1. The purpose of this one is to prevent iframe hijacking and websites that use this script to be prevented from being iframed without permission. */
+ if (window.top !== window.self)
+        {
+            window.top.location.replace(window.self.location.href);
+        }
+
+/* script border!!! Also objectz(or pheebob, not sure) is saying that cubejubejube is doxxing a guy named rexy. REXY IS CUBEJUBEJUBE'S ALT. */
