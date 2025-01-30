803a37624ed90eb2c9e0a11c9250ee61dd879cb0
xdzipper-alt <xdzipperlol89@outlook.com>
2025-01-29 23:01:56 -0500

    Update xdzippersgadget.js

--- xdzippersgadget.js
+++ xdzippersgadget.js
@@ -1,5 +1,5 @@
 /* This is one of XDZIPPER's Gadgets. This one with the ID of 1. The purpose of this one is to prevent iframe hijacking and websites that use this script to be prevented from being iframed without permission. */
 if (window.self !== window.top) {
-    window.self.location.href = 'https://raw.githubusercontent.com/xdzipper-alt/img/refs/heads/main/nooooooo.png';
+    window.self.location.href = 'https://xdzipper-alt.github.io/nooooooo.png';
 }
 
